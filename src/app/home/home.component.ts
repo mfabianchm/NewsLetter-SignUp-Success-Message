377e3bca183ceccf0264956c0794c46bd21e839e
Marcos Chong <mfchongm@hotmail.com>
2024-01-04 13:11:01 -0600

    mobile styles added

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgForm, FormsModule } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Event, Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -10,10 +10,18 @@ import { Router } from '@angular/router';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   constructor(private router: Router) {}
   mail = '';
   error = false;
+  home_img = '../assets/illustration-sign-up-desktop.svg';
+  ngOnInit(): void {
+    if (window.innerWidth < 996) {
+      this.home_img = '../assets/illustration-sign-up-mobile.svg';
+    } else {
+      this.home_img = '../assets/illustration-sign-up-desktop.svg';
+    }
+  }
   save(mail: string, f: NgForm) {
     if (f.invalid) {
       this.error = true;
@@ -22,6 +30,15 @@ export class HomeComponent {
       this.router.navigate(['/success'], {
         state: { value: this.mail },
       });
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (event.target.innerWidth < 996) {
+      this.home_img = '../assets/illustration-sign-up-mobile.svg';
+    } else {
+      this.home_img = '../assets/illustration-sign-up-desktop.svg';
     }
   }
 }
